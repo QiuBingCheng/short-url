cab28be8074a8aaf061b3e9dd42197712c650699
QiuBingCheng <bingchengqiu@protonmail.com>
2024-01-27 11:44:42 +0800

    clear all records in table before init admin data

--- app/commands/init_data.py
+++ app/commands/init_data.py
@@ -1,5 +1,5 @@
 from flask_script import Command
-from app.database.models import User
+from app.database.models import User, UrlMapping, TracingRecord
 from app import app
 from app.lib.db_operation import delete_all_records
 import logging
@@ -9,12 +9,18 @@ class InitDataCommand(Command):
     """ Initialize the data."""
 
     def run(self):
+        clear_data()
         init_data()
+
+
+def clear_data():
+    tables = [TracingRecord, UrlMapping, User]
+    for table in tables:
+        delete_all_records(table)
 
 
 def init_data():
     # create admin data
-    delete_all_records(User)
     user = User(username=app.config["USERNAME"],
                 password=app.config["PASSWORD"],
                 email=app.config["MAIL"], role="admin")
